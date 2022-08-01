a7b95a1febbb9a2b247b0e7929457394969e15ce
gangelo <web.gma@gmail.com>
2022-07-31 21:53:03 -0400

    Refactors to make code more maintainable

--- lib/deco/hash_loadable.rb
+++ lib/deco/hash_loadable.rb
@@ -4,23 +4,30 @@ module Deco
   # Provides methods to load and return information about a given hash.
   module HashLoadable
     include AttributeInformable
-    include AttrAccessorCreatable
     include AttributeValuesAssignable
 
     def load_hash(hash:, options: {})
-      raise ArgumentError, 'hash is blank?' if hash.blank?
-      raise ArgumentError, 'hash is not a Hash' unless hash.is_a? Hash
-      raise ArgumentError, 'options is not a Hash' unless options.is_a? Hash
+      Validators::validate!(hash: hash, options: options)
 
       namespace = [options[:namespace]].compact
       attribute_info = attribute_info_from(hash: hash, namespace: namespace)
       @attribute_info.merge!(attribute_info)
-      attr_accessor_create attribute_names: attribute_info&.keys
       apply_attribute_values_from_hash(hash: hash, attribute_info: attribute_info)
     end
 
     def apply_attribute_values_from_hash(hash:, attribute_info:)
       assign_attribute_values hash: hash, attribute_info: attribute_info
+    end
+
+    # Module that defines validators for the parent module.
+    module Validators
+      class << self
+        def validate!(hash:, options:)
+          raise ArgumentError, 'hash is blank?' if hash.blank?
+          raise ArgumentError, 'hash is not a Hash' unless hash.is_a? Hash
+          raise ArgumentError, 'options is not a Hash' unless options.is_a? Hash
+        end
+      end
     end
   end
 end

44f4fe5b688cba443ef0b63818025eec14d2b1b7
gangelo <web.gma@gmail.com>
2022-08-02 04:30:34 -0400

    Rix rubocop violations

--- lib/deco/attribute_retrievable.rb
+++ lib/deco/attribute_retrievable.rb
@@ -3,7 +3,6 @@
 module Deco
   # Defines methods to retrieve model attribute values dynamically.
   module AttributeRetrievable
-
     # Returns the value of the attribute using fully quaified attribute names.
     def retrieve_attribute_value(hash:, attribute_info:)
       hash.dig(*[attribute_info[:in], attribute_info[:attribute_name]].flatten.compact)

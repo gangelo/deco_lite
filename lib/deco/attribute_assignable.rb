23954dab25432c558359582336db40b90c3945c5
gangelo <web.gma@gmail.com>
2022-08-02 04:06:45 -0400

    Create module AttributeRetrievable
    To retrieve attribute values.

--- lib/deco/attribute_assignable.rb
+++ lib/deco/attribute_assignable.rb
@@ -1,13 +1,16 @@
 # frozen_string_literal: true
 
+require_relative 'attribute_retrievable'
+
 module Deco
   # Defines methods to assign model attribute values dynamically.
   module AttributeAssignable
     include AttributeAccessorCreatable
+    include AttributeRetrievable
 
     def assign_attribute_values(hash:, attribute_info:)
       attribute_info.each do |attribute_name, attr_info|
-        value = get_attribute_value(hash: hash, attribute_info: attr_info)
+        value = retrieve_attribute_value(hash: hash, attribute_info: attr_info)
         assign_attribute_value(attribute_name: attribute_name, value: value)
       end
     end
@@ -16,11 +19,6 @@ module Deco
       # Create our attributes before we send.
       create_attribute_accessor attribute_name: attribute_name
       public_send("#{attribute_name}=", value)
-    end
-
-    # Returns the value of the attribute using fully quaified attribute names.
-    def get_attribute_value(hash:, attribute_info:)
-      hash.dig(*[attribute_info[:in], attribute_info[:attribute_name]].flatten.compact)
     end
   end
 end

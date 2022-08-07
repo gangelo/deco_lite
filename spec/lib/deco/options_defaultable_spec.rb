f528b9afda737f004245d0048eba7fbb947b290d
gangelo <web.gma@gmail.com>
2022-08-07 07:08:33 -0400

    Refactor OptionsDefaultable specs to simpler

--- spec/lib/deco/options_defaultable_spec.rb
+++ spec/lib/deco/options_defaultable_spec.rb
@@ -1,18 +1,7 @@
 RSpec.describe Deco::OptionsDefaultable, type: :module do
-  let(:expected_value) do
-    {
-      Deco::FieldsOptionable::OPTION_FIELDS =>
-        Deco::FieldsOptionable::OPTION_FIELDS_DEFAULT,
-      Deco::NamespaceOptionable::OPTION_NAMESPACE =>
-        Deco::NamespaceOptionable::OPTION_NAMESPACE_DEFAULT
-    }
-  end
-
   describe 'constants' do
-    describe '.DEFAULT_OPTIONS' do
-      it 'returns the default options' do
-        expect(subject::DEFAULT_OPTIONS).to eq expected_value
-      end
+    describe 'DEFAULT_OPTIONS' do
+      it_behaves_like 'a constant', :DEFAULT_OPTIONS, { fields: :merge, namespace: nil }
     end
   end
 end

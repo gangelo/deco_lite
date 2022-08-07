54c14579d151e75709b4cfcd296b6915c7ab16a2
gangelo <web.gma@gmail.com>
2022-08-06 21:32:38 -0400

    Add specs for FieldRetrievable

--- spec/lib/deco/field_retrievable_spec.rb
+++ spec/lib/deco/field_retrievable_spec.rb
@@ -1,9 +1,26 @@
 RSpec.describe Deco::FieldRetrievable, type: :module do
   subject(:klass) do
-    class AwesomeKlass
+    Class.new do
       include Deco::FieldRetrievable
     end.new
   end
 
-  it 'does something awesome'
+  describe 'class methods' do
+    describe '.retrieve_field_value' do
+      let(:hash) { { a: { b: { c: 'value' } } } }
+      let(:field_info)  { { field_name: :c, dig:[:a, :b], namespace: nil } }
+
+      it 'does something' do
+        expect(described_class.retrieve_field_value(hash: hash,
+                                                    field_info:
+                                                    field_info)).to eq 'value'
+      end
+    end
+  end
+
+  describe '#retrieve_field_value' do
+    it 'does not respond_to?' do
+      expect(subject).to_not respond_to :retrieve_field_value
+    end
+  end
 end
